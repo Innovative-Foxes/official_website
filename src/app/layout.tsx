3a8b1900ae2416b25452f1b30c3d4d9a53965cfe
tyrellcurry <tyrellcurryio@gmail.com>
2025-09-01 21:51:10 -0700

    Gtag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter, Poppins } from "next/font/google";
 import type { Metadata } from "next";
 import { Provider } from "@/components/ui/provider";
+import Script from "next/script";
 
 import "./globals.css";
 import { Box } from "@chakra-ui/react";
@@ -32,6 +33,21 @@ export default function RootLayout({
       className={`${inter.variable} ${poppins.variable} antialiased`}
       lang="en"
     >
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17525626678"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17525626678');
+          `}
+        </Script>
+      </head>
       <body>
         <Provider>
           <Box color={"blue"} fontSize={"xl"}>
