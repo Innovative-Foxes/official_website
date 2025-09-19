356cdb3ace9eac6111ba86fb19ddd8e151eb8f4a
tyrellcurry <tyrellcurryio@gmail.com>
2025-09-18 18:50:47 -0700

    Name fix

--- src/app/components/sections/MeetTeam/MeetTeam.tsx
+++ src/app/components/sections/MeetTeam/MeetTeam.tsx
@@ -31,7 +31,7 @@ const MeetTeam = () => {
       name: "Éric Vaillancourt",
       greeting: "Bonjour 👋",
       introduction:
-        "I'm Éric Valiancourt, Senior Email Developer & Graphic Designer.",
+        "I'm Éric Vaillancourt, Senior Email Developer & Graphic Designer.",
       description: [
         "For over a decade, I’ve honed my skills in graphic design and email development. I've mastered the art of creating experiences that not only look great but also perform flawlessly across every device and client. I specialize in platforms such as Marketo, SFMC, and Pardot, helping campaigns reach their full potential.",
         "Along the way, I’ve partnered with hundres of clients and teams such as Air Canada Vacations, Amazon, Bank OZK, HashiCorp, James Hardie, Meta, NYU, S&P Global, Sub-Zero, and many more.",
